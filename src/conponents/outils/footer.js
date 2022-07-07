860a1f4c4260a5fcc9344f06215673d052fd1b1c
omer-alt <wafomer912@gmail.com>
2022-07-07 16:31:56 +0200

    gestion de validite frontent d'adresse mail

--- src/conponents/outils/footer.js
+++ src/conponents/outils/footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import * as FaIcons from "react-icons/fa";
 import * as GoIcons from "react-icons/go";
 import shot1 from '../assets/images/shop-1.jpg'
@@ -13,6 +13,35 @@ import shot8 from '../assets/images/shop-8.jpg'
 import '../../styles/footer.css'
 
 const Footer = () => {
+
+    const [endEmail, setEndEmail] = useState("")
+    const emailRef = useRef("")
+  
+    const handleChange = ()=>{
+      setEndEmail(emailRef.current.value)
+    }
+  
+    function validation(){
+  
+      var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+      document.getElementById('resultat').innerHTML = "";
+      console.log(endEmail)
+      
+  
+      if (expressionReguliere.test(endEmail)){
+  
+        document.getElementById('result').innerHTML = "succes de soumission!";
+        document.getElementById('result').style.color = '#00FF00';
+        emailRef.current.value = "";
+        setEndEmail("")
+      } else {
+  
+        document.getElementById('result').innerHTML = "L'adresse mail n'est pas valide!";
+        document.getElementById('result').style.color = 'red';
+      }
+      return false;
+    }
+
   return (
     <div className='footer'>
         <div className='footertop'>
@@ -58,8 +87,12 @@ const Footer = () => {
                 </div>
                 <div className='fcText'>Sed sit amet nisl in velit viverra bibendum in ac nisi. Etiam efficitur dui vitae sem rutrum,</div>
                 <div className="fcInput">
-                    <input className='fInput' placeholder='INSERT YOUR EMAIL'/>
-                    <button className='fButton'>SUBMIT</button>
+                    
+                    <div style={{display:"flex",flexDirection:"column"}}>
+                        <input ref={emailRef} className='fInput' onChange={handleChange} placeholder='INSERT YOUR EMAIL'/>
+                        <span id='result'></span>
+                    </div>
+                    <button className='fButton' onClick={validation}>SUBMIT</button>
                 </div>
             </div>
             <div className='footerColumn'>
--- src/conponents/outils/newLetter.js
+++ src/conponents/outils/newLetter.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import '../../styles/newletter.css'
 
 const NewLetter = () => {
+
+  const [endEmail, setEndEmail] = useState("")
+  const emailRef = useRef("")
+
+  const handleChange = ()=>{
+    setEndEmail(emailRef.current.value)
+  }
+
+  function validation(){
+
+    var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+    document.getElementById('resultat').innerHTML = "";
+    console.log(endEmail)
+
+    if (expressionReguliere.test(endEmail)){
+
+      document.getElementById('resultat').innerHTML = "succes de soumission!";
+      document.getElementById('resultat').style.color = '#00FF00';
+      emailRef.current.value = "";
+      setEndEmail("")
+    } else {
+
+      document.getElementById('resultat').innerHTML = "L'adresse mail n'est pas valide!";
+      document.getElementById('resultat').style.color = 'red';
+    }
+    return false;
+  }
+
   return (
     <div className='newLetter'>
         <div>
@@ -10,9 +38,17 @@ const NewLetter = () => {
                 Get the 50%  off
             </h1>
         </div>
-        <div className='newLetterRight'>
-            <input placeholder='INSERT YOUR EMAIL'></input>
-            <button className='newLetterButton' type='submit' >SUBMIT</button>
+        <div className='yourMail'>
+          <div className='newLetterRight'>
+            <input ref={emailRef} placeholder='INSERT YOUR EMAIL' onChange={handleChange} id="emailInput"></input>
+            <button 
+              id='newLetterButton'  
+              className='newLetterButton' 
+              type='submit'
+              onClick={validation}
+            >SUBMIT</button>
+          </div>
+          <span id='resultat'></span>
         </div>
     </div>
   )
